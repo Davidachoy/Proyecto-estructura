184b3fd4c73d8a739c0a4c8678a757f1e81a101b
NicoTencio <nico.tencio@estudiantec.cr>
2021-09-03 16:34:47 -0600

    Struct Sharon1

--- main.cpp
+++ main.cpp
@@ -2,13 +2,67 @@
 
 using namespace std;
 
+struct Curso{
+
+    int creditos;
+    string nomCurso;
+    int codigo;
+    Curso*sig;
+    struct Grupo*sublistaGrupos;
+
+    Curso(int c,string nom,int cod){
+    creditos = c;
+    nomCurso = nom;
+    codigo   = cod;
+
+    sig  = NULL;
+    punG = NULL;
+
+    }
+};
+
+struct Grupo{
+    int numGrupo;
+    Grupo*sig;
+    struct Curso*enlaceCurso,*enlaceCursoFinal;
+    struct Evaluacion*tempTC,*tempP,*tempExa,*tempG;
+
+    Grupo(int num){
+    numGrupo = num;
+    sig  = NULL;
+    enlaceCurso = NULL;
+    enlaceCursoFinal = NULL;
+    tempTC = NULL;
+    tempP = NULL;
+    tempExa = NULL;
+    tempG = NULL;
+    }
+
+};
+
+struct Evaluacion{
+    string tipo;
+    int dia;
+    int mes;
+    int year;
+    string horaEntrega;
+    Evaluacion*sig;
+
+    Evaluacion(string type, int day, int month, int a, string hour){
+    tipo        = type;
+    dia         = day;
+    mes         = month;
+    year        = a;
+    horaEntrega = hour;
+
+    sig   = NULL;
+
+    }
+
+};
+
 int main()
 {
-    cout<<"hola";
-    cout<<"";
-    cout<<"Casa";
-    int casa;
-    
     cout << "Hello world!" << endl;
     return 0;
 }
